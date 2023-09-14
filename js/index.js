44f712e05450a13122d1bb079652e9003038da91
Bowie <gusthmp@gmail.com>
2023-09-14 19:33:04 -0300

    Adicionado novos items no cardapio

--- js/index.js
+++ js/index.js
@@ -28,35 +28,55 @@ class Index {
         const areaPizza = document.querySelector('.index__pizza-area')
         const areaLanche = document.querySelector('.index__burguer-area')
         const areaSobremesa = document.querySelector('.index__sobremesa-area')
+        const areaPratos = document.querySelector('.index__pratos-area')
         const btnPizza = document.querySelector('.index__btn-pizza')
         const btnLanche = document.querySelector('.index__btn-lanche')
         const btnSobremesa = document.querySelector('.index__btn-sobremesa')
+        const btnPratos = document.querySelector('.index__btn-pratos')
+
 
         btnPizza.addEventListener('click', () => {
             areaPizza.classList.add('active')
             areaLanche.classList.remove('active')
             areaSobremesa.classList.remove('active')
+            areaPratos.classList.remove('active')
             btnPizza.classList.add('active')
             btnLanche.classList.remove('active')
             btnSobremesa.classList.remove('active')
+            btnPratos.classList.remove('active')
         })
 
         btnLanche.addEventListener('click', () => {
             areaPizza.classList.remove('active')
             areaLanche.classList.add('active')
             areaSobremesa.classList.remove('active')
+            areaPratos.classList.remove('active')
             btnPizza.classList.remove('active')
             btnLanche.classList.add('active')
             btnSobremesa.classList.remove('active')
+            btnPratos.classList.remove('active')
         })
 
         btnSobremesa.addEventListener('click', () => {
             areaPizza.classList.remove('active')
             areaLanche.classList.remove('active')
             areaSobremesa.classList.add('active')
+            areaPratos.classList.remove('active')
             btnPizza.classList.remove('active')
             btnLanche.classList.remove('active')
             btnSobremesa.classList.add('active')
+            btnPratos.classList.remove('active')
+        })
+
+        btnPratos.addEventListener('click', () => {
+            areaPizza.classList.remove('active')
+            areaLanche.classList.remove('active')
+            areaSobremesa.classList.remove('active')
+            areaPratos.classList.add('active')
+            btnPizza.classList.remove('active')
+            btnLanche.classList.remove('active')
+            btnSobremesa.classList.remove('active')
+            btnPratos.classList.add('active')
         })
     }
 
@@ -65,7 +85,6 @@ class Index {
             let pizzaItem = document.querySelector('.index__model .index__pizza-item').cloneNode(true)
             document.querySelector('.index__pizza-area').append(pizzaItem)
     
-            // Preencher os dados de cada pizza
             pizzaItem.querySelector('.index__pizza-item--img img').src = item.img
             pizzaItem.querySelector('.index__pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
             pizzaItem.querySelector('.index__pizza-item--name').innerHTML = item.name
@@ -83,12 +102,9 @@ class Index {
 
     burguer() {
         burguerJson.forEach((item, index ) => {
-            //console.log(item)
             let burguerItem = document.querySelector('.index__model .index__burguer-item').cloneNode(true)
-            //console.log(burguerItem)
             document.querySelector('.index__burguer-area').append(burguerItem)
         
-            // preencher os dados de cada burguer
             burguerItem.querySelector('.index__burguer-item--img img').src = item.img
             burguerItem.querySelector('.index__burguer-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
             burguerItem.querySelector('.index__burguer-item--name').innerHTML = item.name
@@ -102,6 +118,69 @@ class Index {
         })
     }
 
+    pratos() {
+        pratosJson.forEach((item, index ) => {
+            let pratosItem = document.querySelector('.index__model .index__pratos-item').cloneNode(true)
+            document.querySelector('.index__pratos-area').append(pratosItem)
+        
+            pratosItem.querySelector('.index__pratos-item--img img').src = item.img
+            pratosItem.querySelector('.index__pratos-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+            pratosItem.querySelector('.index__pratos-item--name').innerHTML = item.name
+            pratosItem.querySelector('.index__pratos-item--desc').innerHTML = item.description
+
+            if (item.sale === true) {
+                const sale = pratosItem.querySelector('.index__pratos-item .index__sale')
+                sale.classList.add('active')
+            }
+        
+        })
+        
+    }
+
+    startCounterAnimation() {
+        const valueDisplays = document.querySelectorAll('.index__numbers-main');
+    
+        function animateValue(element, start, end, duration) {
+            let current = start;
+            const range = end - start;
+            const increment = end > start ? 1 : -1;
+            const stepTime = Math.abs(Math.floor(duration / range));
+    
+            const timer = setInterval(() => {
+                current += increment;
+                element.textContent = current;
+                if (current === end) {
+                    clearInterval(timer);
+                }
+            }, stepTime);
+        }
+    
+        function handleIntersect(entries, observer) {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const valueDisplay = entry.target;
+                    const startValue = 0;
+                    const endValue = parseInt(valueDisplay.getAttribute('data-val'));
+                    const duration = 2000;
+                    animateValue(valueDisplay, startValue, endValue, duration);
+                    observer.unobserve(valueDisplay);
+                }
+            });
+        }
+    
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        };
+    
+        const observer = new IntersectionObserver(handleIntersect, options);
+    
+        valueDisplays.forEach((valueDisplay) => {
+            observer.observe(valueDisplay);
+        });
+    }
+
 
    
 
@@ -113,6 +192,9 @@ class Index {
         this.filter()
         this.pizza()
         this.burguer()
+        this.pratos()
+        this.startCounterAnimation()
+
         console.log('hello')
 
     }
--- js/items.js
+++ js/items.js
@@ -17,3 +17,12 @@ let burguerJson = [
     {id:5, name:'X-Burguer Triplo Cheddar', sale: false, img:'img/burg5.png',category:"burg", price:29.00, description:'Pão, Maionese, 3 Hamburguers, Queijo Cheddar'},
     {id:6, name:'X-Burguer Tudo', sale: true, img:'img/burg6.png',category:"burg", price:35.00, description:'Pão, Maionese, Hamburguer, Queijo, Alface, Tomate, Bacon, Calabresa, Ovo'},
 ];
+
+let pratosJson = [
+    {id:1, name:'Prato de tilapia Grelhada', sale: false, img:'img/prato1.png',category:"prato", price:25.00, description:'Arroz com Legumes, Batatas cozidas, Salada e Tilapia grelhada'},
+    {id:2, name:'Prato de Bife a Cavalo', sale: false, img:'img/prato2.png',category:"prato", price:26.00, description:'Arroz, Feijão, Batatas fritas, Salada, Bife e Ovo frito'},
+    {id:3, name:'Prato com Carne do Tabajaras', sale: true, img:'img/prato3.png',category:"prato", price:28.00, description:'Arroz Temperado, Salada e Carne do Tabajaras'},
+    {id:4, name:'Prato de Macarrão', sale: false, img:'img/prato4.png',category:"prato", price:30.00, description:'Macarrão e Carne Moida'},
+    {id:5, name:'Prato Peruano', sale: false, img:'img/prato5.png',category:"prato", price:29.00, description:'Arroz Temperado, Salada, Feijao Peruano e Camarão'},
+    {id:6, name:'Prato de Ceviche Atum', sale: true, img:'img/prato6.png',category:"prato", price:35.00, description:'Ceviche Atum com tempero Peruano'},
+];
